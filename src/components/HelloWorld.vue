88471b63433e35f0f26e5c11b909ea22cf5c0c4c
David Robison <mrdavidrobison@gmail.com>
2018-11-07 17:06:10 -0700

    take out unnecessary packages

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,38 +5,15 @@
 </template>
 
 <script>
-import axios from 'axios'
-import dotenv from 'dotenv'
-
-dotenv.config()
-
-let url = process.env.AWS_TEST_URL
-
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      movies: []
-    }
-  },
-  methods: {
-    insertMovie () {
-      axios
-        .get(url).then(response => {
-          this.movie = response.data
-        })
-        .catch(function (error) {
-          console.log(error)
-          return false
-        })
-    }
-  }
+  name: 'HelloWorld'
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
