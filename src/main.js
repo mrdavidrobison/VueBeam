bfb2e8f7ffa2c3c6a2893a893d25d2c8fefcdeeb
David Robison <mrdavidrobison@gmail.com>
2018-11-07 17:10:58 -0700

    begin new aws call with imported properties from env file

--- src/main.js
+++ src/main.js
@@ -3,13 +3,42 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import {
+  AWS_REGION,
+  AWS_ENDPOINT,
+  AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY
+} from '../.env'
+
+var AWS = require('aws-sdk')
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  data () {
+    return {
+      movies: []
+    }
+  },
+  methods: {
+    insertMovie () {
+    }
+  },
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
+
+AWS.config.update({
+  region: AWS_REGION,
+  endpoint: AWS_ENDPOINT,
+  accessKeyId: AWS_ACCESS_KEY_ID,
+  secretAccessKey: AWS_SECRET_ACCESS_KEY
+})
+
+var dynamodb = new AWS.DynamoDB()
+var dynamodoc = new AWS.DynamoDB.DocumentClient()
