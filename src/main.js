9cf408ac2bf3538af20ed60f68534cf5edb4a37a
David Robison <mrdavidrobison@gmail.com>
2018-11-09 16:47:31 -0700

    successfully call dynamodb data for test

--- src/main.js
+++ src/main.js
@@ -1,44 +1,51 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+/* eslint-disable */
+import Vue from 'vue';
 import {
   AWS_REGION,
-  AWS_ENDPOINT,
   AWS_ACCESS_KEY_ID,
-  AWS_SECRET_ACCESS_KEY
-} from '../.env'
-
-var AWS = require('aws-sdk')
+  AWS_SECRET_ACCESS_KEY,
+  AWS_ENDPOINT
+} from "../awsconfig";
+import AWS from "aws-sdk";
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
+const vm = new Vue({
   el: '#app',
-  data () {
+  data() {
     return {
-      movies: []
+      tablesList: []
     }
   },
   methods: {
-    insertMovie () {
+    getImageData() {
+      AWS.config.update({
+        region: AWS_REGION,
+        endpoint: AWS_ENDPOINT,
+        accessKeyId: AWS_ACCESS_KEY_ID,
+        secretAccessKey: AWS_SECRET_ACCESS_KEY
+      });
+
+      let dynamodb = new AWS.DynamoDB();
+      let docClient = new AWS.DynamoDB.DocumentClient();
+
+      let params = {
+        TableName: "ProdImage",
+      };
+
+      dynamodb.describeTable(params, function (err, data) {
+        let tableList = JSON.stringify(data, undefined, 2);
+        if (err) {
+          document.getElementById("app").innerHTML =
+            "Unable to list tables: " +
+            "\n" +
+            JSON.stringify(err, undefined, 2);
+        } else {
+          vm.tablesList = tableList;
+        }
+      });
     }
-  },
-  router,
-  components: {
-    App
-  },
-  template: '<App/>'
-})
-
-AWS.config.update({
-  region: AWS_REGION,
-  endpoint: AWS_ENDPOINT,
-  accessKeyId: AWS_ACCESS_KEY_ID,
-  secretAccessKey: AWS_SECRET_ACCESS_KEY
-})
-
-var dynamodb = new AWS.DynamoDB()
-var dynamodoc = new AWS.DynamoDB.DocumentClient()
+  }
+});
